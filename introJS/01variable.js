1e424c6c86b77687fc4b47a41d1876ffe306e3bc
manishaanand10 <manishaanand461@gmail.com>
2026-01-02 07:55:50 +0000

    container confi done

---
+++ introJS/01variable.js
@@ -0,0 +1,13 @@
+const accountID =2250746412
+var accountEmail="manishaanand@765"
+let accountpassword="isha1230"
+let accountstate;//value undefined
+
+//change the value of key funct
+
+// accountID =2568431546515 //CONST value cannot be change.
+accountEmail ="isha@1000"
+accountpassword ="pick20"
+
+console.table([accountID,accountEmail,accountpassword,accountstate])
+
