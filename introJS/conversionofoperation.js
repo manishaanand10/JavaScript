ccc24078e60722e53359e264c97327113e5bb09a
manishaanand10 <manishaanand461@gmail.com>
2026-01-04 07:04:27 +0000

    conersion of datatype

---
+++ introJS/conversionofoperation.js
@@ -0,0 +1,49 @@
+ let age =null
+//console.log(typeof age);
+//console.log(typeof(age));
+
+let valueInNumber = Number(age)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//null=>0
+//true=>1; false=>0
+//undefined=>underfined
+// "23"=>23
+//"23acd"=>nan
+
+let isLoggedIn = 1 
+let booleanIsLoddegIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoddegIn);
+//console.log(isLoggedIn);
+
+////OPERATIONS///
+
+  let value = 10
+  let negValue = -value
+  //console.log(negValue);
+
+// console.log(2+2);//4 
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**3);// 2the power3 8
+// console.log(2%3);// remedir
+// console.log(2/3);//0.6
+
+let srt1 ="manisha"
+let str2 ="anand"
+
+let srt3= srt1 + str2
+//console.log(srt3);
+
+// console.log(1 + 2);
+// console.log("1" + 2 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+console.log(true);
+
+
+  
+
+
