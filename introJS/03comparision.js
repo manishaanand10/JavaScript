31fa2560fd45ace5ad58078453337b6d43139c10
manishaanand10 <manishaanand461@gmail.com>
2026-01-05 08:22:10 +0000

    comparision of datatype

---
+++ introJS/03comparision.js
@@ -0,0 +1,21 @@
+// console.log(2>=4);//greater then
+// console.log(2<=4);//less then
+// console.log(2=4);//equal to 
+// console.log(2!=4);// notequal  
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//=== check datatype
+console.log("2" === 2);
+console.log("2" === "2");
+console.log( 2 === 2 );
+console.log( 2 === "2" );
+
+
+ 
