c520c65a5f470ab9d30b60ed8cd18d74a4cb0936
manishaanand10 <manishaanand461@gmail.com>
2026-01-07 09:07:44 +0000

    dataypememory

---
+++ introJS/04_memory.js
@@ -0,0 +1,7 @@
+// ***********STACK , HEAP MEMORY *******
+
+// STACK:-PRIMITTIVE TYPE liner datatype structure (lifo :- last in first out)
+
+// HEAP:-NON-PRIMIVITE TYPE special tree-based data structre
+
+let
