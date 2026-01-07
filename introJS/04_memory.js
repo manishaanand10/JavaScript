fb8a36f490ad4ff6484bf26334134dca4c148b98
manishaanand10 <manishaanand461@gmail.com>
2026-01-07 14:12:39 +0000

    stack$heap

--- introJS/04_memory.js
+++ introJS/04_memory.js
@@ -4,4 +4,25 @@
 
 // HEAP:-NON-PRIMIVITE TYPE special tree-based data structre
 
-let
+let username = "manishaanand10"
+
+let anothername = username
+anothername = "micki"
+console.log(anothername);
+console.log(username);
+
+
+let studentOne = {
+    Name:"manishaanand",
+    age :22
+}
+
+let studentTwo = studentOne// same value change.
+studentTwo.name = "riya"
+
+console.log(studentOne.name);
+console.log(studentTwo.name);
+
+
+
+
